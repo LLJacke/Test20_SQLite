70f556deb1fde2e1937a3988ac570e9862f789a5
lljackie <782361105@qq.com>
2016-09-19 18:29:48 +0800

    Initial commit

---
+++ app/src/main/java/com/lljackie/test20_sqlite/MainActivity.java
@@ -0,0 +1,131 @@
+package com.lljackie.test20_sqlite;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    WordsDBHelper mDbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView list = (ListView) findViewById(R.id.lv_words);
+        registerForContextMenu(list);
+
+
+        mDbHelper = new WordsDBHelper(this);
+
+        //在列表显示全部单词
+        ArrayList<Map<String, String>> items = getAll();
+        setWordsListView(items);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mDbHelper.close();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_search:
+                //查找
+                SearchDialog();
+                return true;
+            case R.id.action_insert:
+                //新增单词
+                InsertDialog();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.contextmenu_wordslistview, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        TextView textId=null;
+        TextView textWord=null;
+        TextView textMeaning=null;
+        TextView textSample=null;
+
+        AdapterView.AdapterContextMenuInfo info=null;
+        View itemView=null;
+
+        switch (item.getItemId()){
+            case R.id.action_delete:
+                //删除单词
+                info=(AdapterView.AdapterContextMenuInfo)item.getMenuInfo();
+                itemView=info.targetView;
+                textId =(TextView)itemView.findViewById(R.id.textId);
+                if(textId!=null){
+                    String strId=textId.getText().toString();
+                    DeleteDialog(strId);
+                }
+                break;
+            case R.id.action_update:
+                //修改单词
+                info=(AdapterView.AdapterContextMenuInfo)item.getMenuInfo();
+                itemView=info.targetView;
+                textId =(TextView)itemView.findViewById(R.id.textId);
+                textWord =(TextView)itemView.findViewById(R.id.textViewWord);
+                textMeaning =(TextView)itemView.findViewById(R.id.textViewMeaning);
+                textSample =(TextView)itemView.findViewById(R.id.textViewSample);
+                if(textId!=null && textWord!=null && textMeaning!=null && textSample!=null){
+                    String strId=textId.getText().toString();
+                    String strWord=textWord.getText().toString();
+                    String strMeaning=textMeaning.getText().toString();
+                    String strSample=textSample.getText().toString();
+                    UpdateDialog(strId, strWord, strMeaning, strSample);
+                }
+                break;
+        }
+        return true;
+    }
+
+
+    private void setWordsListView(ArrayList<Map<String, String>> items) {
+
+    }
+
+    private ArrayList<Map<String, String>> getAll() {
+        return null;
+    }
+
+    private void InsertDialog() {
+
+    }
+
+    private void SearchDialog() {
+
+    }
+
+}
